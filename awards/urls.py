5836248e374e25e52e9100163e7974d7f5b36551
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-18 05:18:01 +0300

    Add the projects url file

--- awards/urls.py
+++ awards/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+    # path('', include('api.urls')),
+    path('',include('projects.urls')),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
