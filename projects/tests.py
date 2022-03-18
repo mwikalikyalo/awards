0ec8e065eb0380d63474b328fdfea6443bd13f7b
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-18 08:48:15 +0300

    Add the new project

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,4 @@
+import email
 from django.test import TestCase
 from .models import Profile, Project 
 from django.contrib.auth.models import User
@@ -5,28 +6,28 @@ from django.contrib.auth.models import User
 # Create your tests here.
 class ProfileTestClass(TestCase):
     def setUp(self):
-        self.user = User(username = 'rue')
+        self.user = User(username = 'mwikali')
         self.user.save()
-        self.ruweydha = Profile(user = self.user, id = self.user.profile.id, bio = 'I love coding', profile_pic = 'rue.png', contact = '0767543211')
+        self.mwikali = Profile(user = self.user, id = self.user.profile.id, bio = 'You are doing great.', profile_photo = 'default.png', email= 'winniemwikali07@gmail.com')
         
     def tearDown(self):
         Profile.objects.all().delete()
         User.objects.all().delete()        
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.ruweydha, Profile))
+        self.assertTrue(isinstance(self.mwikali, Profile))
 
     def test_saveProfile(self):
-        self.ruweydha.save_profile()
+        self.mwikali.save_profile()
         profile_saved = Profile.objects.all()
         self.assertTrue(len(profile_saved) > 0)
 
 
 class ProjectTestClass(TestCase):
     def setUp(self):
-        self.user=User(username='aisha')
+        self.user=User(username='chichi')
         self.user.save()
-        self.project=Project(image='default.png',title = 'Insta clone',description='A clone of instagram.',profile=self.user.profile, project_url = 'instagram.com')   
+        self.project=Project(image='default.png',title = 'photo-django',description='This is a Django gallery project which shows different images of different locations and categories.',profile=self.user.profile, project_url = 'https://photo-00001.herokuapp.com/')   
 
     def tearDown(self):
         Project.objects.all().delete()
