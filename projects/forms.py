632826f9973c34ec74869b5cf29cf285f3598eaf
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-19 05:11:50 +0300

    Add the forms

---
+++ projects/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Project, Profile
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model =  Project
+        exclude = [ 'design']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['username']
