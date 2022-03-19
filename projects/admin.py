eb89942b2319d5d51889ca58e902205550534cb5
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-19 17:13:14 +0300

    Add the registration form

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Profile, Project
+from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Project)
+admin.site.register(User)
