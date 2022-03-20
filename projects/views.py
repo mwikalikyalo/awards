9557fe6eb14acfb28c619c39b9bc0c936cfba41a
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-20 05:04:02 +0300

    Add the api views

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import theProfiles, theProjects
+from .serializer import MerchSerializer
 
 # Create your views here.
-def home(request):
-  return render(request, 'home.html')
+class MerchList(APIView):
+    def get(self, request, format=None):
+        all_merch = theProjects.objects.all()
+        serializers = MerchSerializer(all_merch, many=True)
+        return Response(serializers.data)
+
+class MerchList(APIView):
+    def get(self, request, format=None):
+        all_merch = theProfiles.objects.all()
+        serializers = MerchSerializer(all_merch, many=True)
+        return Response(serializers.data)
+
