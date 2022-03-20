ce118142f13ef2162c500ac98724c331e68a442f
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-20 16:54:05 +0300

    Add the status as a response

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import theProfiles, theProjects
 from .serializer import MerchSerializer
+from rest_framework import status
 
 # Create your views here.
 
@@ -11,11 +12,11 @@ class MerchList(APIView):
     def get(self, request, format=None):
         all_merch = theProjects.objects.all()
         serializers = MerchSerializer(all_merch, many=True)
-        return Response(serializers.data)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
 
 class MerchList(APIView):
     def get(self, request, format=None):
         all_merch = theProfiles.objects.all()
         serializers = MerchSerializer(all_merch, many=True)
-        return Response(serializers.data)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
 
